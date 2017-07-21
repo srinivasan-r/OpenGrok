63f51e9845a33fe1460e75f5e18bff9bde9529bc
srinivasan-r <srinivasanr.cs@gmail.com>
2017-07-21 13:04:29 +0530

    Init

---
+++ opengrokindex.bat
@@ -0,0 +1,2 @@
+java -jar C:\softwares\BigData\opengrok-1.1-rc5\lib\opengrok.jar -W C:\softwares\BigData\opengrok-1.1-rc5\configuration.xml -c C:\softwares\BigData\ctags58\ctags.exe -P -S -v -s C:\Viewstore\Cerber_SSA_2.5.1 -d C:\softwares\BigData\opengrok-1.1-rc5\grokdata
+pause
